4af6a8919a9f124e840cbad96fa68ddc468a519a
RandDarras <randdarras@yahoo.com>
2020-04-24 18:20:54 +0300

    Modified Stop Watch

--- w3-d6.js
+++ w3-d6.js
@@ -19,28 +19,61 @@ var eject= function(){
 	if(toast===undefined||toast===""){
 		return "Empty, Add Toast!"
 	} 
-	this.addToast=undefined
+	this.toast=undefined
 	return toast
 }
 
 
 var start= function (seconds){
-var toast=this.toast
-var ding =function (){console.log("Ding!")}
+
+	var toast=this.toast
+	var ding =function (){console.log("Ding!")}
+
 	if (toast===""||toast===undefined){
-		return "Empty, Add toast"
-	
+		return "Empty, Add toast";
 	} else if(toast.indexOf("toasted!")>=0){
 				var toastedIndex=toast.indexOf("toasted!")
 				this.timeOut= setTimeout(ding,(seconds*1000))
 				this.toast=toast.slice(0,toastedIndex)+"Burnt!"
-			} else {
-				this.timeOut= setTimeout(ding,(seconds*1000))
-				this.toast+= " toasted!"
-			}
-		}
+	} else {
+		this.timeOut= setTimeout(ding,(seconds*1000))
+		this.toast+= " toasted!"
+	}
+}
 
 var stop =function(){
  clearTimeout(this.timeOut)
 }
 
+function makeStopwatch() {
+	var instance={}
+	instance.time=0
+	instance.start=start
+	instance.id=0
+	instance.stop=stop
+	instance.reset=reset
+	return instance
+}
+
+
+var stop=function(){
+	return clearInterval(this.id)
+}
+
+var reset=function(){
+	clearInterval(this.id)
+	return this.time=0
+}
+
+var timer=function() {
+ 		this.time+=1;
+ 		console.log('Elapsed time: ' + this.time + 's.');
+ 	}
+
+var start=function(){
+	var timer=function() {
+ 		this.time+=1;
+ 		console.log('Elapsed time: ' + this.time + 's.');
+ 	}
+ 	this.id=setInterval(timer,1000)
+}
